a78fbda31bfdd2d0a8c614876523bf9000a1ca10
EpicViv <70602891+EpicViv@users.noreply.github.com>
2021-01-20 10:25:13 -0600

    Finished day 1 notes (Jan 20, 2021)

--- ArrayNotes.java
+++ ArrayNotes.java
@@ -1,10 +1,49 @@
 
 /**
- * Write a description of class Notes here.
+ * Notes for the Arrays Unit.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Vivek Ily
+ * @version January 20, 2021
  */
-public class ArrayNotes
-{
+public class ArrayNotes{
+
+    public static void createArrayOfEvens(){
+        /*
+
+        An array is an ordered collection of elements of the same type. The type can be a primitive type or a class.
+
+        An array variable is like an object variable in that it must be declared and initialized.
+
+        The number in the square brackets specifies the number of elements in the array.
+
+        All elements in the array are initialized to their default values (e.g. 0, false, null).
+
+        This code creates an array that contains 10 int values (all i to start).
+         */
+
+        int[] evens = new int[10];
+
+        for (int i = 0; i < evens.length; i++) {
+            evens[i] = (i + 1) * 2;
+        }
+
+        System.out.println(evens);
+
+        for (int i = 0; i < evens.length; i++) {
+            System.out.println(i + ": " + evens[i]);
+        }
+    }
+
+    public static void createArrayOfOdds(){
+        int[] odds = new int[] {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+
+        for (int i = 0; i <= odds.length; i++) {
+            System.out.println(i + ": " + odds[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        createArrayOfEvens();
+    }
+
 }
