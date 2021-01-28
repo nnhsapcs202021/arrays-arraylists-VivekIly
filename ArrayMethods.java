559193280d41b13eba3babd9a1e2ef4b28ecb030
EpicViv <70602891+EpicViv@users.noreply.github.com>
2021-01-28 15:05:55 -0600

    Update ArrayMethods.java

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -1,30 +1,25 @@
 import java.util.Arrays;
 
-public class ArrayMethods
-{
+public class ArrayMethods {
     private int[] values;
 
-    public ArrayMethods(int[] initialValues)
-    {
+    public ArrayMethods(int[] initialValues) {
         this.values = initialValues;
     }
 
-    public void printArray()
-    {
+    public void printArray() {
         System.out.println(Arrays.toString(this.values));
     }
 
-    public void swapFirstAndLast()
-    {
-        int first =  this.values[0];
+    public void swapFirstAndLast() {
+        int first = this.values[0];
         int last = this.values[this.values.length - 1];
 
         this.values[0] = last;
         this.values[this.values.length - 1] = first;
     }
 
-    public void shiftRight()
-    {
+    public void shiftRight() {
         int[] newArray = new int[this.values.length];
         for (int i = 0; i < this.values.length; i++) {
             if (i != this.values.length - 1) {
@@ -36,96 +31,83 @@ public class ArrayMethods
         this.values = newArray;
     }
 
-    public void evenToZero()
-    {
+    public void evenToZero() {
         for (int i = 0; i < this.values.length; i++) {
-            if (this.values[i] % 2 == 0)
-            {
+            if (this.values[i] % 2 == 0) {
                 this.values[i] = 0;
             }
         }
     }
 
-    public void largerOfNeighbours()
-    {
-        for (int i = 1; i < this.values.length - 1; i++)
-        {
+    public void largerOfNeighbours() {
+        for (int i = 1; i < this.values.length - 1; i++) {
             int lessIndex = this.values[i - 1];
             int moreIndex = this.values[i + 1];
 
-            if (lessIndex > moreIndex) 
-            {
+            if (lessIndex > moreIndex) {
                 this.values[i] = lessIndex;
-            }
-
-            else 
-            {
+            } else {
                 this.values[i] = moreIndex;
             }
         }
     }
 
-    public void removeMiddle()
-    {
-        if (this.values.length % 2 == 0)
-        {
+    public void removeMiddle() {
+        if (this.values.length % 2 == 0) {
             int middle = (this.values.length / 2) - 1;
 
             int[] temp = new int[this.values.length - 2];
 
-            for (int i = 0; i < temp.length; i++)
-            {
+            for (int i = 0; i < temp.length; i++) {
                 int index = 0;
-                if (i != middle || i != middle + 1)
-                {
+                if (i != middle || i != middle + 1) {
                     index = i;
                     temp[i] = this.values[index];
-                }
-
-                else
-                {
+                } else {
                     index++;
                 }
             }
-        }
-
-        else
-        {
+        } else {
             int middle = (this.values.length / 2);
 
             int[] temp = new int[this.values.length - 1];
 
             int index = 0;
-            for (int i = 0; i < temp.length; i++)
-            {
-                if (i != middle)
-                {
+            for (int i = 0; i < temp.length; i++) {
+                if (i != middle) {
                     index = i;
                     temp[i] = this.values[index];
-                }
-                
-                else
-                {
+                } else {
                     index++;
                 }
             }
         }
     }
 
-    public static void main( String[] args )
-    {
-        int[] testValues = new int[]{ 3, 5, 6, 1 /* add your own values */ };
-        ArrayMethods tester = new ArrayMethods( testValues );
+    public static void main(String[] args) {
+        int[] testValues = new int[]{3, 5, 6, 1 /* add your own values */};
+        ArrayMethods tester = new ArrayMethods(testValues);
         tester.printArray();
         tester.swapFirstAndLast();
         tester.printArray();
 
-        testValues = new int[]{ 1, 6, 3, 9, 5, 6 /* add your own values */ };
+        testValues = new int[]{1, 6, 3, 9, 5, 6 /* add your own values */};
         tester = new ArrayMethods(testValues);
         tester.printArray();
         tester.shiftRight();
         tester.printArray();
 
-       
+        tester.evenToZero();
+        tester.printArray();
+
+        testValues = new int[]{1, 6, 3, 9, 5, 6 /* add your own values */};
+        tester.largerOfNeighbours();
+        tester.printArray();
+
+        testValues = new int[]{1, 6, 3, 9, 5, 6 /* add your own values */};
+        tester.removeMiddle();
+        tester.printArray();
+
+
     }
 }
